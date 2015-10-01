eed94c89c320f7c4589b9c9e3e4655dede5df308
Langley, Dwayne <DLangley@DIRECTV.com>
2015-10-01 10:00:42 -0700

    added notes

--- StupidMathPuzzles/MyPlayground.playground/Contents.swift
+++ StupidMathPuzzles/MyPlayground.playground/Contents.swift
@@ -70,3 +70,50 @@ var a = "6", b = "75", c = "67"
 pMath.add(c.ints, addend: a.ints)
 pMath.addL(b.ints, addend: c.ints)
 
+/*
+
+Addition
+
+Terms Used in Addition Key
+In addition, an augend and an addend are added to find a sum.
+
+In the following equation, 6 is the augend, 3 is the addend, and 9 is the sum:
+
+6 + 3 = 9
+
+NOTE: Sometimes both the augend and addend are called addends. Sometimes the sum is called the total.
+
+Subtraction
+
+Terms Used in Subtraction Key
+In subtraction, a subtrahend is subtracted from a minuend to find a difference.
+
+In the following equation, 9 is the minuend, 3 is the subtrahend, and 6 is the difference.
+
+9 – 3 = 6
+
+Multiplication
+
+Terms Used in Multiplication Key
+In multiplication, a multiplicand and a multiplier are multiplied to find a product.
+
+In the following equation, 6 is the multiplicand, 3 is the multiplier, and 18 is the product.
+
+6 x 3 = 18
+
+NOTE: Sometimes the multiplicand and the multiplier are both called factors.
+
+Division
+
+Terms Used in Division Key
+In division, a dividend is divided by a divisor to find a quotient.
+
+In the following equation, 18 is the dividend, 3 is the divisor, and 6 is the quotient.
+
+18 / 3 = 6
+
+If there is an amount left over, it is called the remainder. The remainder cannot be evenly divided by the divisor. For example, if you divide 18 by 7, you will get a remainder:
+
+18 / 7 = 2, with a remainder of 4
+
+*/
